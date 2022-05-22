83e392a2d7a8cd718a932fde30b80978077b2a3b
mccd326 <jonathanseitz04@web.de>
2022-05-22 20:53:19 +0200

    Redesign
    The text input field is no longer used for file selection. Instead the simple "JFileChooser" takes this task from now on. But there isn't a file filter till now...

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         musicPlayerForm musicPlayerForm = new musicPlayerForm();
         musicPlayerForm.setVisible(true);
         musicPlayerForm.setTitle("Music Player");
-        musicPlayerForm.setBounds(300,300,700,600);
+        musicPlayerForm.setBounds(300,300,700,150);
         musicPlayerForm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         musicPlayer Player = musicPlayer.getInstance();
     }
